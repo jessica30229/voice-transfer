95ec076bd149fa33e9d6ba01959dcffd49a70ab2
Jessica Jiang <jessica30229@gmail.com>
2022-06-07 11:12:02 +0800

    delete

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React, { useRef, useState, useEffect } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 import "./App.css";
 import microPhoneIcon from "./img/microphone.svg";
-import output_audio from "./converted_audio/output.wav";
+import output_audio from "./static/output.wav";
 
 function App() {
   const [isListening, setIsListening] = useState(false);
+  const [isTransformed, setIsTransformed] = useState(false);
   const microphoneRef = useRef(null);
   useEffect(() => {
     const sendAudioFile = async () => {
@@ -43,8 +44,23 @@ function App() {
     microphoneRef.current.classList.remove("listening");
     stopRecording();
   };
+
+  const HttpsProxyAgent = require("https-proxy-agent");
+  var proxy = {
+    protocol: "http:",
+    host: "127.0.0.1",
+    port: 5000,
+  };
+  const options = {};
+  if (proxy) {
+    options.agent = new HttpsProxyAgent(proxy);
+  }
   const handleTransfer = () => {
-    fetch(`/audioUpload/${mediaBlobUrl}`)
+    fetch(`audioUpload/${mediaBlobUrl}`, options)
+      // ,
+      //   {
+      //     agent: new HttpsProxyAgent("http://127.0.0.1:5000"),
+      //   }
       .then((res) => {
         return res.blob();
       })
@@ -55,41 +71,46 @@ function App() {
       .catch((e) => {
         /*發生錯誤時要做的事情*/
       });
+    setIsTransformed(true);
   };
-
+  const stopHandleTransfer = () => {
+    setIsTransformed(false);
+  };
   console.log(mediaBlobUrl);
   return (
     <div className="microphone-wrapper">
       <div className="mircophone-title">
         Welcome to the world of voice changing!
       </div>
-      <div className="mircophone-container">
-        <div
-          className="microphone-icon-container"
-          ref={microphoneRef}
-          onClick={handleListing}
-        >
-          <img
-            src={microPhoneIcon}
-            alt="microPhoneIcon"
-            className="microphone-icon"
-          />
-        </div>
-        <div className="microphone-status">
-          {isListening ? "Listening........." : "Click to start Listening"}
-        </div>
-        {isListening && (
-          <button
-            className="microphone-stop btn"
-            onClick={() => {
-              stopHandle();
-            }}
+      {!isTransformed && (
+        <div className="mircophone-container">
+          <div
+            className="microphone-icon-container"
+            ref={microphoneRef}
+            onClick={handleListing}
           >
-            Stop
-          </button>
-        )}
-      </div>
-      {mediaBlobUrl && (
+            <img
+              src={microPhoneIcon}
+              alt="microPhoneIcon"
+              className="microphone-icon"
+            />
+          </div>
+          <div className="microphone-status">
+            {isListening ? "Listening........." : "Click to start Listening"}
+          </div>
+          {isListening && (
+            <button
+              className="microphone-stop btn"
+              onClick={() => {
+                stopHandle();
+              }}
+            >
+              Stop
+            </button>
+          )}
+        </div>
+      )}
+      {!isTransformed && mediaBlobUrl && (
         <div className="microphone-result-container">
           <video
             className="microphone-result-audio"
@@ -97,14 +118,24 @@ function App() {
             controls
             loop
           />
+          <button className="microphone-transfer btn" onClick={handleTransfer}>
+            start to transfer
+          </button>
+        </div>
+      )}
+      {isTransformed && (
+        <div className="microphone-result-container">
           <audio
             className="microphone-result-audio"
             src={output_audio}
             controls
             loop
           />
-          <button className="microphone-transfer btn" onClick={handleTransfer}>
-            start to transfer
+          <button
+            className="microphone-transfer btn"
+            onClick={stopHandleTransfer}
+          >
+            back to transfer
           </button>
         </div>
       )}
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import convert_realtime
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "static/"
 
 @app.route('/')
 def root():
     return "ROOT!"
+
 @app.route('/audioUpload/<in_url>')
 def audioUpload(in_url):
+    print(in_url)
     #in_url = "https://drive.google.com/uc?export=download&id=1TZPfZkzCyO3Wb9_EjEzJ5ZEqsGU4PenC";
-    out_url = "../client/src/converted_audio/"
+    out_url = "../client/static"
     convert_realtime.web_voice_convert(in_url=in_url, out_url=out_url)
-    return "done!"
-'''
-@app.route('/getname',method=['GET'])
-def getname():
-    name = request.args.get('name')
-    return render_template('get.html',**locals())
-'''
+    # return "../flask-server/static/output.wav"
+
 if __name__ == '__main__':
     app.run(debug=True)
